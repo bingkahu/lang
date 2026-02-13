de8ccb6522df4c3742d3efad8c046fe2882362fa
Matteo Grassi <mgrassi1@outlook.com>
2026-02-13 23:28:48 +0000

    Update data.r

--- data.r
+++ data.r
@@ -1,3 +1,13 @@
-x <- c(1, 2, 3, 4, 5)
-print(mean(x))
-summary(x)
+# Simulation Data
+player_data <- data.frame(
+  id = 1:50,
+  score = rnorm(50, mean=500, sd=100),
+  active = sample(c(TRUE, FALSE), 50, replace=TRUE)
+)
+
+print("Calculating Statistical Average...")
+mean_score <- mean(player_data$score)
+print(paste("Mean Score:", mean_score))
+
+summary(player_data)
+plot(player_data$score)
