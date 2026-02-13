c862a3774a2acaa9998186b9ad1a7f83d77925fe
Matteo Grassi <mgrassi1@outlook.com>
2026-02-13 23:27:37 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,18 @@
-print("Lua Script Running")
-for i=1,10 do
-  print("Count: " .. i)
+local game_engine = {
+    version = "2.0.1",
+    author = "BabelMaster",
+    active = true
+}
+
+function process_logic(val)
+    if val > 10 then
+        return val * 2
+    else
+        return val + 10
+    end
+end
+
+for i = 1, 50 do
+    local result = process_logic(i)
+    print("Lua Calculation " .. i .. ": " .. result)
 end
